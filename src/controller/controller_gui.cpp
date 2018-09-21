7b2a2a0bc9e83ecfb284678c8e60b55d9cf4807d
Emmanuel Durand <emmanueldurand@gmail.com>
2018-09-21 18:09:01 -0400

    Adding support for compiling the GUI with Emscripten

--- src/controller/controller_gui.cpp
+++ src/controller/controller_gui.cpp
@@ -869,13 +869,13 @@ void Gui::initImGui(int width, int height)
     glGetProgramiv(_imGuiShaderHandle, GL_LINK_STATUS, &status);
     if (status == GL_FALSE)
     {
-        Log::get() << Log::WARNING << "Shader::" << __FUNCTION__ << " - Error while linking the shader program" << Log::endl;
+        Log::get() << Log::WARNING << "Gui::" << __FUNCTION__ << " - Error while linking the shader program" << Log::endl;
 
         GLint length;
         glGetProgramiv(_imGuiShaderHandle, GL_INFO_LOG_LENGTH, &length);
         char* log = (char*)malloc(length);
         glGetProgramInfoLog(_imGuiShaderHandle, length, &length, log);
-        Log::get() << Log::WARNING << "Shader::" << __FUNCTION__ << " - Error log: \n" << (const char*)log << Log::endl;
+        Log::get() << Log::WARNING << "Gui::" << __FUNCTION__ << " - Error log: \n" << (const char*)log << Log::endl;
         free(log);
 
         // TODO: handle this case...
